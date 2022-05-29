226a0b8f8139ed7ecede73db6c638c0ac15fb630
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-05-29 14:05:35 +0000

    update

--- tests/blockquote/blockquote.js
+++ tests/blockquote/blockquote.js
@@ -3,6 +3,7 @@ const { REGEXP_BLOCKQUOTE } = require('../../src/index');
 describe('RegEx: REGEXP_BLOCKQUOTE', () => {
 
     test('should match the expected ...', () => {
+        console.log('123');
     });
 
     // test('', () => {
