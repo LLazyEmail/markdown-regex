daf6078d494f701c8b799ee37c8e7801e8648177
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-06-02 20:03:13 +0300

    Update _to-try.js

--- src/_to-try.js
+++ src/_to-try.js
@@ -5,14 +5,12 @@
 //   {regex: /\[([^\[]+)\]\(([^\)]+)\)/g, replacement: '<a href=\'$2\'>$1</a>'},        // hyperlink
 //   {regex: /(\*\*|__)(.*?)\1/g, replacement: '<strong>$2</strong>'},                  // bold
 //   {regex: /(\*|_)(.*?)\1/g, replacement: '<em>$2</em>'},                             // emphasis
-//   {regex: /\~\~(.*?)\~\~/g, replacement: '<del>$1</del>'},                           // del
-//   {regex: /\:\"(.*?)\"\:/g, replacement: '<q>$1</q>'},                               // quote
+
 //   {regex: /`(.*?)`/g, replacement: '<code>$1</code>'},                               // inline code
 //   {regex: /\n\*(.*)/g, replacement: ulList},                                         // ul lists
 //   {regex: /\n[0-9]+\.(.*)/g, replacement: olList},                                   // ol lists
 //   {regex: /\n(&gt;|\>)(.*)/g, replacement: blockquote},                              // blockquotes
 //   {regex: /\n-{5,}/g, replacement: '\n<hr />'},                                      // horizontal rule
-//   {regex: /\n([^\n]+)\n/g, replacement: para},                                       // add paragraphs
 //   {regex: /<\/ul>\s?<ul>/g, replacement: ''},                                        // fix extra ul
 //   {regex: /<\/ol>\s?<ol>/g, replacement: ''},                                        // fix extra ol
 //   {regex: /<\/blockquote><blockquote>/g, replacement: '\n'}                          // fix extra blockquote
