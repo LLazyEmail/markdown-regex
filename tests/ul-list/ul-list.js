9f896df3d38b3b4c359331098753114f22859d5c
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-06-04 13:02:55 +0000

    connecting md

--- tests/ul-list/ul-list.js
+++ tests/ul-list/ul-list.js
@@ -1,11 +1,16 @@
-// const { resolve } = require('path')
-// const root = resolve(__dirname, '')
-
+const { resolve } = require('path')
 const { REGEXP_UL_LIST } = require('../../src/index');
+
+
+const root = resolve(__dirname, '')
+
 
 describe('RegEx: REGEXP_UL_LIST', () => {
 
     test('should match the expected ...', () => {
+
+        
+
     });
     
    // test('', () => {
