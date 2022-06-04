72c9aa68b0958799b545c9eef706af67d27312e3
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-06-04 08:31:12 +0000

    changes

--- tests/blockquote/blockquote.js
+++ tests/blockquote/blockquote.js
@@ -1,11 +1,13 @@
 // const { resolve } = require('path')
 // const root = resolve(__dirname, '')
 
-const { REGEXP_BLOCKQUOTE } = require('../../src/index');
+const { REGEXP_BLOCKQUOTE, REGEXP_EMPTY_BLOCKQUOTE } = require('../../src/index');
 
 describe('RegEx: REGEXP_BLOCKQUOTE', () => {
 
     test('should match the expected ...', () => {
+        let result = REGEXP_BLOCKQUOTE.test('> this is blockquote');
+        expect(result).toBe(true);
     });
    // test('', () => {
    // });
