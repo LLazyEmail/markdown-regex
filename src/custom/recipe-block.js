0d929ecb948661a7cf314849b4c7c766eaa15e20
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-07-25 01:22:55 +0300

    Update recipe-block.js

--- src/custom/recipe-block.js
+++ src/custom/recipe-block.js
@@ -1,1 +1,13 @@
-//xxx
+import os from "os";
+import { REGEXP_IMAGE, REGEXP_LINK, REGEXP_H2, REGEXP_H3 } from './index.js';
+
+
+const platform = os.platform();
+
+const newLine = platform === "win32" ? "\r\n" : "\n";
+
+
+//const REGEXP_UL_LIST = new RegExp(
+//  `${newLine}(((\\s{4})?\\*(.*?)${newLine}){1,})`,
+//  "g"
+//);
