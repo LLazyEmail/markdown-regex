96f67f5ed9cc82cde86e8fd1f9044e7f21d658f9
Olha Alekseeva <108941080+Olgalex@users.noreply.github.com>
2022-08-21 14:52:43 +0300

    Update em.test.js

--- tests/em/em.test.js
+++ tests/em/em.test.js
@@ -6,7 +6,7 @@ const { REGEXP_EM } = require('../../src/index');
 describe('RegEx: REGEXP_EM', () => {
 
     test('should match the expected ...', () => {
-      let result = REGEXP_EM.test('## [Linkedin page of LLazyEmail](https://www.linkedin.com/company/llazyemail/)');
+      let result = REGEXP_EM.test("# Magic Behind Test Automation");
       expect(result).toBe(true);
     });
 
