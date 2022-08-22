204cc134a991f13f546aa6292ec6dccdbebe2690
Olha Alekseeva <108941080+Olgalex@users.noreply.github.com>
2022-08-22 11:55:34 +0300

    Update paragraph.test.js

--- tests/paragraph/paragraph.test.js
+++ tests/paragraph/paragraph.test.js
@@ -6,7 +6,7 @@ const { REGEXP_PARAGRAPH } = require('../../src/index');
 describe('RegEx: REGEXP_PARAGRAPH', () => {
 
     test('should match the expected ...', () => {
-      let result = REGEXP_PARAGRAPH.test('## [Linkedin page of LLazyEmail](https://www.linkedin.com/company/llazyemail/)');
+      let result = REGEXP_PARAGRAPH.test(' ');
       expect(result).toBe(true);
     });
 
