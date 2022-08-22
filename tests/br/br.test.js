54507e75b62ba8b0788c0072774ba9497d462267
Olha Alekseeva <108941080+Olgalex@users.noreply.github.com>
2022-08-22 11:39:30 +0300

    Update br.test.js

--- tests/br/br.test.js
+++ tests/br/br.test.js
@@ -6,7 +6,7 @@ const { REGEXP_BR } = require('../../src/index');
 describe('RegEx: REGEXP_BR', () => {
 
     test('should match the expected ...', () => {
-      let result = REGEXP_BR.test("**AD Link: [(https://bit.ly/3pGj1Zq)]**");
+      let result = REGEXP_BR.test("**AD Link: [(https://bit.ly/3pGj1Zq)]</br>AD Copy: Buy NordVPN's 2-year plan and get +3 months for free. **");
       expect(result).toBe(true);
     });
 
