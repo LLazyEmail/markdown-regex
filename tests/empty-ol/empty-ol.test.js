c8f8cb99e953c62213eb0637956ff336569a7575
Olha Alekseeva <108941080+Olgalex@users.noreply.github.com>
2022-08-22 11:45:15 +0300

    Update empty-ol.test.js

--- tests/empty-ol/empty-ol.test.js
+++ tests/empty-ol/empty-ol.test.js
@@ -6,7 +6,7 @@ const { REGEXP_EMPTY_OL } = require('../../src/index');
 describe('RegEx: REGEXP_EMPTY_OL', () => {
 
     test('should match the expected ...', () => {
-      let result = REGEXP_EMPTY_OL.test('## [Linkedin page of LLazyEmail](https://www.linkedin.com/company/llazyemail/)');
+      let result = REGEXP_EMPTY_OL.test(' ');
       expect(result).toBe(true);
     });
 
