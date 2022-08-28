440933c57fadc7f7249984fc1b54b40f35b245d3
Olha Alekseeva <108941080+Olgalex@users.noreply.github.com>
2022-08-28 09:33:34 +0300

    Update empty-ul.test.js

--- tests/empty-ul/empty-ul.test.js
+++ tests/empty-ul/empty-ul.test.js
@@ -6,7 +6,7 @@ const { REGEXP_EMPTY_UL } = require('../../src/index');
 describe('RegEx: REGEXP_EMPTY_UL', () => {
 
     test('should match the expected ...', () => {
-      let result = REGEXP_EMPTY_UL.test(' ');
+      let result = REGEXP_EMPTY_UL.test('1.');
       expect(result).toBe(true);
     });
 
