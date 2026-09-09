4c04117937fc2d7f697620758bcf394f8979ddbd
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2026-09-09 04:23:38 +0300

    fix: update import path in header.test.js to use src/tags/header

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,15 +1,27 @@
 // Tests for headers
 const assert = require('assert');
-const markdownRegex = require('../src/markdownRegex');
+const { REGEXP_HEADER, REGEXP_H2, REGEXP_H3 } = require('../src/tags/header');
 
 describe('Header Tests', () => {
     it('should match h1 headers', () => {
         const input = '# Header 1';
-        assert.ok(markdownRegex.h1.test(input));
+        assert.ok(REGEXP_HEADER.test(input));
     });
     it('should not match invalid headers', () => {
         const input = 'Header 1';
-        assert.ok(!markdownRegex.h1.test(input));
+        assert.ok(!REGEXP_HEADER.test(input));
+    });
+    it('should match h3 header', () => {
+        const input = '### Header 3';
+        assert.ok(REGEXP_H3.test(input));
+    });
+    it('should not match h2 header', () => {
+        const input = '## Header 2';
+        assert.ok(REGEXP_H2.test(input));
+    });
+    it('should not match h1 header', () => {
+        const input = '# Header 1';
+        assert.ok(REGEXP_HEADER.test(input));
     });
     // Add more tests for other header levels and edge cases.
 });
